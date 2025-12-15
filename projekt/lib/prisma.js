916c03f698708875d82fa6a68e06e14d11406a72
kacper-olenkiewicz <20470@student.ans-elblag.pl>
2025-12-15 14:28:42 +0100

    Use Prisma Accelerate

--- projekt/lib/prisma.js
+++ projekt/lib/prisma.js
@@ -2,7 +2,9 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis;
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  accelerateUrl: process.env.PRISMA_DATABASE_URL,
+});
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
